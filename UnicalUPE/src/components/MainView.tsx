33b91a478e038080a9105d1d5a01a2349ad3fa91
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-11-30 21:29:40 -0300

    MainView component created

---
+++ UnicalUPE/src/components/MainView.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { StyleSheet, Button} from 'react-native';
+
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+import Colors from '../constants/Colors';
+
+export default function NavigationScreen({ navigation, children }: RootTabScreenProps<'Navigation'>) {
+
+  return (
+    <View style={styles.back}>
+      <View style={styles.container}>
+        <View>
+          {children}
+        </View>
+      </View>
+      <View style={styles.navigation}>
+    
+      </View>      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 16,
+    borderTopStartRadius: 0,
+    borderTopEndRadius: 0,
+    //marginBottom: 20
+
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  back: {
+    flex:1,
+    backgroundColor: Colors.dark.background
+  },
+  navigation:{
+    flex:1,
+    backgroundColor: Colors.dark.background
+  }
+});
