4f0909e3df504fe620ea460f37b6e29bdf54466f
Clara626 <clara.v.araujo626@gmail.com>
2021-11-30 22:42:47 -0300

    profile screen implemented

--- UnicalUPE/src/screens/NotificationsScreen.tsx
+++ UnicalUPE/src/screens/NotificationsScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import Button from '../../components/Button';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
