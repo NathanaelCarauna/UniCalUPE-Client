6d516da4d4de339b44911417c4917edd3944ff45
Clara626 <clara.v.araujo626@gmail.com>
2021-12-01 11:50:09 -0300

    login screen without logo

--- UnicalUPE/src/screens/LoginScreen.tsx
+++ UnicalUPE/src/screens/LoginScreen.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, Button, Alert, LogBox } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import Colors from '../constants/Colors';
 
 export default function LoginScreen() {
   return (
+    <>
     <View style={styles.container}>
-      <Text style={styles.title}>Login Screen</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabThreeScreen.tsx" />
+      <Text style={styles.title}>Login</Text>
+      <Button 
+      title="  Sign in with google  "
+      color="#a9a9a9"
+      onPress={()=>Alert.alert('Logando com sua conta Google')}
+      />
     </View>
+    </>
+   
   );
 }
 
@@ -19,10 +26,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: Colors.dark.background,
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: 'bold',
+    marginBottom:100,
   },
   separator: {
     marginVertical: 30,
