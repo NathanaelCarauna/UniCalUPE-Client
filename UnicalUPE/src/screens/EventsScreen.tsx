95b274de54972cf80d334d15908b52a334d7cebf
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-01 23:01:39 -0300

    Day's events screen 60% implemented

--- UnicalUPE/src/screens/EventsScreen.tsx
+++ UnicalUPE/src/screens/EventsScreen.tsx
@@ -1,24 +1,75 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import EventComponent from '../components/EventComponent';
 import MainView from '../components/MainView';
 import { Text, View } from '../components/Themed';
 import TitleMainScreen from '../components/TitleMainScreen';
+import Colors from '../constants/Colors';
+
+const fakeData = [
+  {
+    id: '1',
+    title: "Palestra",
+    local: 'Auditorio',
+    presentor: 'Claudio',
+    course: 'Engenharia de software',
+    category: 'SURVEY'
+  },
+  {
+    id: '2',
+    title: "Workshop",
+    local: 'Online',
+    presentor: 'Felipe',
+    course: 'Licenciatura',
+    category: 'COURSE'
+  },
+  {
+    id: '3',
+    title: "Mini curso",
+    local: 'Online',
+    presentor: 'Fernando',
+    course: 'Publico',
+    category: 'PUBLIC'
+  },
+  {
+    id: '4',
+    title: "Mini curso",
+    local: 'Online',
+    presentor: 'Fernando',
+    course: 'Publico',
+    category: 'PUBLIC'
+  },
+]
 
 export default function EventsScreen() {
   return (
-    <MainView>
-       <TitleMainScreen title='Eventos do Dia'/>
-    </MainView>
+    <View style={styles.back}>
+      <View style={styles.container}>
+        <View>
+          <TitleMainScreen title='Eventos do Dia' />
+          <FlatList
+            data={fakeData}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (<EventComponent event={item} />)}
+            style={styles.eventList}
+          />
+        </View>
+      </View>
+      <View style={styles.navigation}>
+
+      </View>
+    </View>      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 5,
+    borderRadius: 16,
+    borderTopStartRadius: 0,
+    borderTopEndRadius: 0,
   },
   title: {
     fontSize: 20,
@@ -29,4 +80,16 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  eventList: {
+    margin: 10,
+    height: '85%',    
+  },
+  back: {
+    flex: 1,
+    backgroundColor: Colors.dark.background
+  },
+  navigation: {
+    flex: 1,
+    backgroundColor: Colors.dark.background
+  }
 });
