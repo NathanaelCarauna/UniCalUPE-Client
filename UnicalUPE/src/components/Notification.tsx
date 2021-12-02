edf76e343634a87db778643e2f0a3571a062d3e4
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-02 12:31:53 -0300

    Notification component adjsted

--- UnicalUPE/src/components/Notification.tsx
+++ UnicalUPE/src/components/Notification.tsx
@@ -15,24 +15,26 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { FontAwesome } from '@expo/vector-icons';
 
 type PropsButton = {
-    
+
     navigation: CompositeNavigationProp<BottomTabNavigationProp<RootTabParamList, "Navigation">, NativeStackNavigationProp<RootStackParamList, string>>
     Text: string,
     destination: string
 }
 
-export default function Notification(props:PropsButton) {    
+export default function Notification(props: PropsButton) {
     const buttonText = props.Text
     const navigate = () => {
         props.navigation.navigate(props.destination)
     }
     return (
         <TouchableOpacity style={styles.bloco} onPress={navigate}>
-            <LinearGradient style={styles.flex} colors={["#2AB75A","#A0FFA3"]}>
-                <TabBarIcon style={styles.options} name="ellipsis-v" color={'white'} />
-                <Text style={styles.title}>
-                    {buttonText}
-                </Text>
+            <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} style={styles.flex} colors={["#2AB75A", "#A0FFA3"]}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>
+                        {buttonText}
+                    </Text>
+                    <TabBarIcon style={styles.options} name="ellipsis-v" color={'white'} />
+                </View>
                 <Text style={styles.data}>
                     00/00/00
                 </Text>
@@ -44,43 +46,53 @@ export default function Notification(props:PropsButton) {
 function TabBarIcon(props: {
     name: React.ComponentProps<typeof FontAwesome>['name'];
     color: string;
-  }) {
+}) {
     return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
-  }
+}
 
 const styles = StyleSheet.create({
-    
+
     title: {
-        alignSelf:'flex-start',
-        marginBottom:10,
+        flex: 1,
+        alignSelf: 'flex-start',
+        // marginBottom:10,
         marginStart: 20,
         fontSize: 20,
         fontWeight: 'bold',
         color: 'white'
     },
-    data:{
+    data: {
         alignSelf: 'flex-end',
-        marginEnd:20,
+        marginEnd: 20,
         marginBottom: 10,
         color: Colors.blue.background
     },
-    bloco:{
+    bloco: {
         alignSelf: 'stretch',
         backgroundColor: Colors.Red.background,
         margin: 10,
         marginBottom: 10,
         borderRadius: 16,
         overflow: 'hidden',
-        
-      },
-    options:{
-        alignSelf: 'flex-end',
-        marginEnd: 15,
-        marginTop:15,
-        fontSize: 20,
+
+    },
+    titleContainer: {        
+        flex: 1,
+        marginVertical: 8,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center'
         
     },
-    flex:{
-        alignItems:'baseline'
+    options: {
+        // alignSelf: 'flex-end',
+        marginEnd: 15,
+        // marginTop: 15,
+        fontSize: 20,        
+    },
+    flex: {
+        alignItems: 'baseline'
     }
 })
