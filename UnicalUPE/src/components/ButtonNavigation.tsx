7cfcc1f506927448d16bc6b9d554aa12a88620f7
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-02 13:08:16 -0300

    Changing button detail color dinamically

--- UnicalUPE/src/components/ButtonNavigation.tsx
+++ UnicalUPE/src/components/ButtonNavigation.tsx
@@ -7,6 +7,10 @@ import Layout from '../constants/Layout';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 import ButtonDetail from '../../assets/images/ButtonDetail.svg';
+import ButtonDetailRed from '../../assets/images/ButtonDetailRed.svg';
+import ButtonDetailBlue from '../../assets/images/ButtonDetailBlue.svg';
+import ButtonDetailGreen from '../../assets/images/ButtonDetailGreen.svg';
+import ButtonDetailYellow from '../../assets/images/ButtonDetailYellow.svg';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { CompositeNavigationProp } from '@react-navigation/native';
@@ -20,7 +24,7 @@ type PropsButton = {
     destination: string
 }
 
-export default function ButtonNavigation(props:PropsButton) {    
+export default function ButtonNavigation(props: PropsButton) {
     const buttonText = props.buttonText
     const backgroundColor = props.backColor || Colors.Orange.background
     //const details = props.detailColor 
@@ -28,11 +32,30 @@ export default function ButtonNavigation(props:PropsButton) {
         props.navigation.navigate(props.destination)
     }
     return (
-        <TouchableOpacity 
-            style={[styles.navButton, {backgroundColor: backgroundColor}]}
+        <TouchableOpacity
+            style={[styles.navButton, { backgroundColor: backgroundColor }]}
             onPress={navigate}
         >
-            <ButtonDetail fill='#123' style={styles.buttonDetail} />
+            {props.backColor == Colors.Red.background 
+            ? <ButtonDetailRed fill='#123' style={styles.buttonDetail} /> 
+            : <></>
+            }
+            {props.backColor == Colors.Blue.background 
+            ? <ButtonDetailBlue fill='#123' style={styles.buttonDetail} /> 
+            : <></>
+            }
+            {props.backColor == '' 
+            ? <ButtonDetail fill='#123' style={styles.buttonDetail} /> 
+            : <></>
+            }
+            {props.backColor == Colors.Green.background
+            ? <ButtonDetailGreen fill='#123' style={styles.buttonDetail} /> 
+            : <></>
+            }
+            {props.backColor == Colors.Yellow.background
+            ? <ButtonDetailYellow fill='#123' style={styles.buttonDetail} /> 
+            : <></>
+            }
             <Text style={styles.buttonName}>
                 {buttonText}
             </Text>
@@ -41,13 +64,13 @@ export default function ButtonNavigation(props:PropsButton) {
     )
 }
 
-function SetDetail(){
+function SetDetail() {
 
 }
 
 const styles = StyleSheet.create({
     navButton: {
-        flex:1,
+        flex: 1,
         backgroundColor: Colors.Orange.background,
         alignItems: 'center',
         justifyContent: 'flex-start',
