583ab1b8c7eb46eb59f16410e01daa004ebbc493
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-02 22:00:04 -0300

    Login screen adjusted

--- UnicalUPE/src/screens/LoginScreen.tsx
+++ UnicalUPE/src/screens/LoginScreen.tsx
@@ -1,41 +1,62 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import * as React from 'react';
-import { StyleSheet, Image, Button, Alert, LogBox } from 'react-native';
+import { StyleSheet, Image, Button, Alert, LogBox, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View, } from '../components/Themed';
 import Colors from '../constants/Colors';
-
 export default function LoginScreen() {
   return (
-    <>
-    <View style={styles.container}>
+    <LinearGradient 
+                style={styles.container}
+                colors={['#fff', Colors.dark.background]}
+            >
+      <Image source={require('./Unical-Logo.png')} style={styles.logo} />
+      {/* <UnicalLogo style={styles.logo} /> */}
       <Text style={styles.title}>Login</Text>
-      <Button 
-      title="  Sign in with google  "
-      color="#a9a9a9"
-      onPress={()=>Alert.alert('Logando com sua conta Google')}
-      />
-    </View>
-    </>
-   
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => Alert.alert('Logando com sua conta Google')}>
+        <Text style={styles.buttonText}>Sign in with google</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 10,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: Colors.dark.background,
+    backgroundColor: '#004369',
   },
   title: {
     fontSize: 40,
     fontWeight: 'bold',
-    marginBottom:100,
+    // marginBottom: 200,
+  },
+  button: {
+    margin: 40,
+    fontWeight: 'bold',
+    backgroundColor: 'white',
+    borderRadius: 15
+  },
+  buttonText: {
+    // margin: 40,
+    fontWeight: 'bold',
+    padding: 15,
+    paddingHorizontal: 40,
+    
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  logo: {
+    // backgroundColor: 'black',
+    width: 200,
+    height: 200
+  }
 });
