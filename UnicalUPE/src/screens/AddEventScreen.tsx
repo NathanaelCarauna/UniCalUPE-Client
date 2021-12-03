5c0f967ede4469a086180e99dedfdc541f594f45
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-02 21:59:15 -0300

    Add event screen adjusted

--- UnicalUPE/src/screens/AddEventScreen.tsx
+++ UnicalUPE/src/screens/AddEventScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, ScrollView, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import EditScreenInfo from '../components/EditScreenInfo';
@@ -10,112 +10,111 @@ import Layout from '../constants/Layout';
 
 export default function Evento() {
   return (
-    <ScrollView style={styles.scroll}>
-        <LinearGradient style = {styles.container} colors={["#ffffff","#B7F2F4"]}>
-          <View style={styles.header}>
-            <View style={{display:'flex', flexDirection: 'row', alignItems:'baseline',  backgroundColor: 'transparent'}}>
-              <TextInput style={styles.title} placeholder="Nome do Evento"/>
-              <TextInput style={styles.text} placeholder="Categoria"/>
-            </View>
-            <View style={{display:'flex', flexDirection: 'row', alignItems:'baseline', marginTop: 10,  backgroundColor: 'transparent'}}>
-              <TabBarIcon name="circle" color={Colors.Blue.background} />
-              <TextInput style={styles.text} placeholder="Apresentador"/>
-            </View>
-            <View style={{display:'flex', flexDirection: 'row', alignItems:'baseline',  backgroundColor: 'transparent'}}>
-              <TabBarIcon name="map-marker" color={Colors.Blue.background} />
-              <TextInput style={styles.text} placeholder="Local"/>
-            </View>
-          </View>
-          <View style={styles.separator} lightColor="#004369" darkColor="rgba(0,67,105,0.1)" />
-            <TextInput style={styles.text} placeholder="Descrição do evento"/>
-            <TextInput style={styles.text} placeholder="link"/>
-        </LinearGradient>
-    </ScrollView>
-   
+    <LinearGradient style={styles.container} colors={["#ffffff", "#8F98FF"]}>
+      <ScrollView style={styles.scroll}>
+
+        <TextInput style={styles.text} placeholder="Nome do Evento" />
+        <TextInput style={styles.text} placeholder="Categoria" />
+
+        <View style={styles.textView}>
+          <TabBarIcon style={styles.icons} name="user" color={Colors.Blue.background} />
+          <TextInput style={styles.text} placeholder="Apresentador" />
+        </View>
+        <View style={styles.textView}>
+          <TabBarIcon style={styles.icons} name="map-marker" color={Colors.Blue.background} />
+          <TextInput style={styles.text} placeholder="Local" />
+        </View>
+
+        <View style={styles.separator} lightColor="#004369" darkColor="rgba(0,67,105,0.1)" />
+        <TextInput style={styles.text} multiline={true} placeholder="Descrição do evento" />
+        <TextInput style={styles.text} placeholder="link" />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => Alert.alert('Evento salvo com sucesso')}>
+          <Text style={styles.buttonText}>Salvar</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </LinearGradient>
+
   );
-  
+
 }
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={30}  {...props} />;
 }
 
 
 const styles = StyleSheet.create({
-  scroll:{
-    backgroundColor: 'black', 
+  scroll: {
+    backgroundColor: 'transparent',
     flex: 1
   },
   container: {
-    height: Layout.window.height - 80,
-    //alignSelf:'stretch',
-    alignItems: 'center',
+    flex: 1,
+    padding: 20,
+    alignItems: 'stretch',
+
     //justifyContent: 'center',
   },
-  title: {
-    fontSize: 30,
-    padding:20,
-    //width:110,
-    //height:40,
-    color: "#60D0D6",
-    fontWeight: 'bold',
-    backgroundColor: '#E9E9E9',
-    borderRadius: 16,
-    borderColor: 'white',
-    borderWidth: 3
-
-  },
-  sub_tittle:{
-    fontSize: 15,
-    marginStart: 10,
-    padding:20,
-    color: 'gray',
-    backgroundColor: '#E9E9E9',
-    borderRadius: 16,
-    borderColor: 'white',
-    borderWidth: 3
-  },
   separator: {
-    marginVertical: 15,
+    margin: 15,
     height: 1,
-    width: '80%',
+    alignSelf: 'stretch',
+    //width: '80%',
     color: "#004369",
   },
-  normal: {
-    fontSize: 18,
-    justifyContent:'center',
-    textAlign: 'center',
-    color: 'white',
-    padding: 15
-  },
-  bloco:{
-    backgroundColor: Colors.Red.background,
-    margin: 10,
-    marginBottom: 10,
-    borderRadius: 16,
-    overflow: 'hidden'
-  },
-  header:{
-    display:'flex',
+  header: {
+    display: 'flex',
     flexWrap: 'wrap',
-    alignSelf:'stretch',
-    alignItems:'flex-start',
-    margin: 20,
+    alignSelf: 'stretch',
+    alignItems: 'flex-start',
+    //justifyContent: 'space-between',
+    //margin: 20,
     backgroundColor: 'transparent'
   },
-  text:{
-    fontSize: 15,
-    marginStart: 10,
+  text: {
+    fontSize: 18,
+    //marginStart: 10,
     marginVertical: 5,
-    padding:15,
+    padding: 12,
     color: 'gray',
     backgroundColor: '#E9E9E9',
     borderRadius: 16,
-    alignSelf:'stretch',
+    alignSelf: 'stretch',
     textAlign: 'center',
     borderColor: 'white',
     borderWidth: 3
-  }
+  },
+  icons: {
+    padding: 10,
+    marginTop: 5,
+    alignSelf: 'flex-start',
+    alignContent: 'flex-start',
+  },
+  textView: {
+    flexDirection: 'row',
+    marginTop: 10,
+    // alignSelf: 'stretch',
+    backgroundColor: 'transparent',
+    justifyContent: 'space-evenly',
+    alignSelf: 'flex-start',
+  },
+  button: {
+    margin: 40,
+    fontWeight: 'bold',
+    borderRadius: 15,
+    alignItems: 'center',
+    backgroundColor: "#8F98FF",
+    borderColor: 'white',
+    borderWidth: 3
+  },
+  buttonText: {    
+    fontWeight: 'bold',
+    padding: 15,
+    fontSize: 18,
+    color: 'white'
+  },
 });
