296e10f1b33b49b1b5cdcb84468b3f30bb5ba427
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-12 16:00:16 -0300

    Rendering course name

--- UnicalUPE/src/screens/ProfileScreen.tsx
+++ UnicalUPE/src/screens/ProfileScreen.tsx
@@ -4,16 +4,19 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 import { Text, View } from '../components/Themed';
 import Colors from '../constants/Colors';
+import AppContext from '../contexts/appContext';
+import { useContext } from 'react';
 
 
 export default function CalendarScreen() {
+  const {user} = useContext(AppContext)
   return (
     <>
       <LinearGradient style = {styles.container} colors={["#ffffff","#ffc278"]}>
         <LinearGradient style={styles.bloco} colors={["#FF7648","#ffc278"]}>
-          <Text style={styles.normal}>Aluno Santos da Silva</Text>
-          <Text style={styles.normal}>Engenharia de Software</Text>
-          <Text style={styles.tipoLogin}>Estudante</Text>
+          <Text style={styles.normal}>{user.name}</Text>
+          <Text style={styles.normal}>{user.course.name}</Text>
+          <Text style={styles.tipoLogin}>{user.accountType}</Text>
           </LinearGradient>
       </LinearGradient>
 
