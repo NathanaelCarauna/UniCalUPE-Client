39ac81a4db974f70dd3e999dcb369d3e0d5677fb
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-12 16:09:40 -0300

    userAPI getUser service implemented

--- UnicalUPE/src/services/userApi.ts
+++ UnicalUPE/src/services/userApi.ts
@@ -1,5 +1,9 @@
 import api from './apiBase';
 
 export function getUser(email){
-    return new api.get('/user', {email})
+    return new api.get(`/user/${email}`)
+}
+
+export function createUser(user){
+    return new api.post('/user', user)
 }
