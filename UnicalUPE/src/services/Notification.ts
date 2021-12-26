9b0845d7e72e782edf596705c76a9b2507bcdbcc
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-26 16:34:32 -0300

    File renamed

--- UnicalUPE/src/services/Notification.ts
+++
@@ -1,9 +0,0 @@
-import api from './apiBase';
-
-export function getAllNotifications(){
-    return new api.get(`/notification/`)
-}
-
-export function GetNotificationByCategory(user){
-    return new api.put(`/notification/findByUser/${user}`)
-}
