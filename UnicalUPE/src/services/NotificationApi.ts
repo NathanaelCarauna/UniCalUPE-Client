4c2081480d50a995d3c7cd02265a17840dc3f60b
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-27 10:25:55 -0300

    Get by category endpoint added

--- UnicalUPE/src/services/NotificationApi.ts
+++ UnicalUPE/src/services/NotificationApi.ts
@@ -1,17 +1,17 @@
 import api from './apiBase';
 
-export function getAllNotifications(){
+export function getAllNotifications() {
     return new api.get(`/notification/`)
 }
 
-export function getNotificationByUserEmail(userEmail:string){
+export function getNotificationByUserEmail(userEmail: string) {
     return new api.get(`/notification/findByUserEmail/${userEmail}`)
 }
 
-export function GetNotificationByCategory(user){
-    return new api.put(`/notification/findByUser/${user}`)
+export function GetNotificationByCategory(userEmail: string, category: string) {
+    return new api.get(`/notification/findByCategory/${userEmail}/${category}`)
 }
 
-export function updateNotification(notification){
+export function updateNotification(notification) {
     return new api.put(`/notification/${notification.id}`, notification)
 }
