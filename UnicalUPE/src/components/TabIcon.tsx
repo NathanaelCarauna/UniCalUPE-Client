6886f70ca33d589074f1cbc3769022c3d23da392
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-12-31 08:57:17 -0300

    Size prop added

--- UnicalUPE/src/components/TabIcon.tsx
+++ UnicalUPE/src/components/TabIcon.tsx
@@ -1,8 +1,10 @@
 import { FontAwesome } from "@expo/vector-icons";
+import React from "react";
 
 export default function TabBarIcon(props: {
     name: React.ComponentProps<typeof FontAwesome>['name'];
     color: string;
+    size: number;
   }) {
-    return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+    return <FontAwesome size={props.size || 30} style={{ marginBottom: -3 }} {...props} />;
   }
