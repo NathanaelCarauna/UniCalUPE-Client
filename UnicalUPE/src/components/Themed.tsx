7351d8e97eb88ffc9e4716dd3dc57916ce4bad0e
Clara626 <clara.v.araujo626@gmail.com>
2022-01-03 17:23:12 -0300

    code typing changes

--- UnicalUPE/src/components/Themed.tsx
+++ UnicalUPE/src/components/Themed.tsx
@@ -8,6 +8,7 @@ import { Text as DefaultText, View as DefaultView } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
+import { TextProps, ViewProps } from '../types';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
@@ -22,14 +23,6 @@ export function useThemeColor(
     return Colors[theme][colorName];
   }
 }
-
-type ThemeProps = {
-  lightColor?: string;
-  darkColor?: string;
-};
-
-export type TextProps = ThemeProps & DefaultText['props'];
-export type ViewProps = ThemeProps & DefaultView['props'];
 
 export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
